ea5d90030515ebda06358bc89acd0c5f159dbcb1
InfernalVortex <pranz10@gmail.com>
2017-11-08 03:10:14 -0500

    TreesAndGraphs

--- CrackingTheCodingInterview/src/TreesAndGraphs/_4_2.java
+++ CrackingTheCodingInterview/src/TreesAndGraphs/_4_2.java
@@ -1,7 +1,35 @@
 package TreesAndGraphs;
 
-/**
- * Created by Pranav on 11/7/2017.
- */
+class TreeNode {
+    int value;
+    TreeNode right;
+    TreeNode left;
+
+    TreeNode( int v ) {
+        this.value = v;
+    }
+}
+
+//can wrap this with
+class Tree {
+    TreeNode root;
+}
+
 public class _4_2 {
+    //given sorted array create BST with minimal height
+    TreeNode createMinimalBST( int array[] ) {
+        return createMinimalBST( array, 0, array.length - 1 );
+    }
+
+    TreeNode createMinimalBST( int array[], int start, int end ) {
+        if( end < start )
+            return null;
+
+        int mid = ( start + end ) / 2;
+        TreeNode n = new TreeNode( array[mid] );
+        n.left = createMinimalBST( array, start, mid - 1 );
+        n.right = createMinimalBST( array, mid + 1, end );
+
+        return n;
+    }
 }
---
+++ CrackingTheCodingInterview/src/TreesAndGraphs/_4_5.java
@@ -0,0 +1,7 @@
+package TreesAndGraphs;
+
+/**
+ * Created by Pranav on 11/8/2017.
+ */
+public class _4_5 {
+}
